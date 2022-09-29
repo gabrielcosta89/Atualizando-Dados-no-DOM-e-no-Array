a0becebb86a1ee0cfd4fa3db9a703119b40e3616
gabrielcosta89 <110191746+gabrielcosta89@users.noreply.github.com>
2022-09-29 14:55:08 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const students = [
+  {
+    name: "Camila",
+    age: 40,
+  },
+  {
+    name: "Carlos",
+    age: 32,
+  },
+  {
+    name: "Antonio",
+    age: 24,
+  },
+  {
+    name: "Luana",
+    age: 36,
+  },
+];
+
+const addNewStudentInDOM = () => {
+
+}
+
+const newStudent = () =>{
+
+}
+
